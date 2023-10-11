b1a71ab484ea1811d973ff709a92efa5cfa1e849
Donghyeon Kim <0916dhkim@gmail.com>
2023-10-11 01:29:52 -0400

    Required input fields

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -25,6 +25,7 @@ export default function Register() {
         name="email"
         type="email"
         placeholder="Email"
+        required
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
@@ -32,6 +33,7 @@ export default function Register() {
         name="password"
         type="password"
         placeholder="Password"
+        required
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
