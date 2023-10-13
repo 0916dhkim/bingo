84aba2936e5c01e5206ef166127341d7a6d0c8c2
Donghyeon Kim <0916dhkim@gmail.com>
2023-10-13 00:07:50 -0400

    prettier init

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -50,7 +50,7 @@ export default async function GamePage({ params }: { params: { id: string } }) {
               </div>
             ) : (
               <div key={index} className={styles.cell} />
-            )
+            ),
           )}
         </div>
       </div>
